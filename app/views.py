a5fa7276ac937979dbeb00b488f37136b1c1b426
Minjeong Shin <minjeongshin@Minjeongs-iMac.local>
2017-08-08 12:18:01 +1000

    sort by combined value, fix rank column

--- app/views.py
+++ app/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, render_to_response
 import os, json
+from random import randint
+
+cur_path = os.path.dirname(os.path.abspath(__file__))
+instmap = None
+
+def findInstitution(inst):
+    global instmap
+    if instmap == None:
+        instmap = {}
+        memberlist = open(os.path.join(cur_path, "data/Member_List.txt"))
+        for l in memberlist.readlines():
+            name, key = l.split('\t')
+            instmap[key.strip()] = name.strip('"')
+    if inst in instmap:
+        return instmap[inst]
+    else:
+        return inst
+
 
 def main(request):
-    cur_path = os.path.dirname(os.path.abspath(__file__))
     rawdata = open(os.path.join(cur_path, "data/aamas2007affil.txt"))
     lines = rawdata.readlines()
     data = []
-    rank = 1
     for i in range(2, len(lines), 3):
         uname = lines[i].split(' ', 1)[1].strip()
         citation = float(lines[i+1].split(' ', 1)[1].strip())
-        data.append((rank, uname, (100-rank), citation, 0))
-        rank += 1
+        data.append((findInstitution(uname), randint(10, 400), citation, 0))
     return render(request, "main.html", {"data": data})
